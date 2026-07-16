0565d3cb2fae8fefa26a9805479c15072c3250bd
ChunYu8866 <asas90804@gmail.com>
2026-07-16 22:10:04 +0800

    release: configure event check-in deployment

--- web/assets/js/config.js
+++ web/assets/js/config.js
@@ -1,7 +1,7 @@
 export const APP_CONFIG = Object.freeze({
-  bridgeUrl: '',
-  bridgeOrigin: 'https://script.googleusercontent.com',
+  bridgeUrl: "https://script.google.com/macros/s/AKfycbwuHzCpxanoLAlVoVOM4RaUYfBnJC801eJvX0RKjcMRzX6zaDTBzCMMmls9aQCHEC6eqw/exec",
+  bridgeOrigin: "https://script.googleusercontent.com",
   walkInEnabled: false,
   privacyNoticeApproved: false,
-  privacyNoticeText: '',
+  privacyNoticeText: "",
 });
